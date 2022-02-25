510fd1f46c9d704893da929cade39123395fc09f
Bisrat Jenbere <jenberehenok@gmail.com>
2022-02-25 13:09:46 -0800

    hourly employee added

---
+++ Hourlyemployee.java
@@ -0,0 +1,12 @@
+class Hourlyemployee {
+    static int hour;
+    static int payment;
+
+    public static void main(String[] args) {
+
+        int wage = hour * payment;
+
+        System.out.println(wage);
+
+    }
+}
